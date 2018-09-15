073a962c3711a92ec5a61b79ed6fee50e7ce493f
Wang Zitao <417301709@qq.com>
2018-09-16 01:37:35 +0800

    补充上传TreeNode

---
+++ MyLeetCode/src/main/java/com/kyrie/myleetcode/model/TreeNode.java
@@ -0,0 +1,16 @@
+package com.kyrie.myleetcode.model;
+
+/**
+ * Created by Tao
+ * Date: 2018/9/15
+ */
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
