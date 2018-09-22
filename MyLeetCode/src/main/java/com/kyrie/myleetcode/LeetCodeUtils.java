ebae82ec465d946beda48d2853524d96d11a9bcf
Wang Zitao <417301709@qq.com>
2018-09-23 00:07:27 +0800

    LeetCode 108 将有序数组转换为二叉搜索树（修复上次上传的bug）

--- MyLeetCode/src/main/java/com/kyrie/myleetcode/LeetCodeUtils.java
+++ MyLeetCode/src/main/java/com/kyrie/myleetcode/LeetCodeUtils.java
@@ -760,7 +760,7 @@ public class LeetCodeUtils {
         return node;*/
 
         //solution2
-        return sortedArrayToBST(nums, 0, nums.length);
+        return sortedArrayToBST(nums, 0, nums.length - 1);
     }
 
     private TreeNode sortedArrayToBST(int[] nums, int start, int end) {
@@ -771,7 +771,7 @@ public class LeetCodeUtils {
         } else {
             int mid = (start + end) / 2;
             TreeNode node = new TreeNode((nums[mid]));
-            node.left = sortedArrayToBST(nums, start, mid);
+            node.left = sortedArrayToBST(nums, start, mid - 1);
             node.right = sortedArrayToBST(nums, mid + 1, end);
             return node;
         }
