2736743a21703451ee621dc6e89c22bf1205bab5
wangzitao <417301709@qq.com>
2019-03-08 16:37:34 +0800

    实现上半圆角矩形

--- app/src/main/java/wzt/mycode/MainActivity.java
+++ app/src/main/java/wzt/mycode/MainActivity.java
@@ -2,6 +2,7 @@ package wzt.mycode;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.ComposeShader;
@@ -11,6 +12,7 @@ import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.RadialGradient;
+import android.graphics.RectF;
 import android.graphics.Shader;
 import android.graphics.Xfermode;
 import android.support.v7.app.AppCompatActivity;
@@ -47,5 +49,33 @@ public class MainActivity extends AppCompatActivity {
         canvas.drawRect(0, 0, 200, 200, paint);
         paint.setXfermode(null);
 //        imageView.setImageBitmap(result);
+
+
+/*        if (source == null) return null;
+        Bitmap result = pool.get(outWidth, outHeight, Bitmap.Config.ARGB_8888);
+        if (result == null) {
+            result = Bitmap.createBitmap(outWidth, outHeight, Bitmap.Config.ARGB_8888);
+        }
+        Matrix matrix = new Matrix();
+        matrix.setScale((float) outWidth / (float) source.getWidth(), (float) outHeight / (float) source.getHeight());
+        Canvas canvas = new Canvas(result);
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        //1.先画圆角矩形的图片
+        BitmapShader bitmapShader = new BitmapShader(source, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+        bitmapShader.setLocalMatrix(matrix);
+        paint.setShader(bitmapShader);
+        canvas.drawRoundRect(new RectF(0, 0, outWidth, outHeight), 20, 20, paint);
+
+        //2.再通过DST_IN画渐变透明效果(用两个LinearGradient是由于最右侧不够全透明，仍然会有边框显示，所以叠加一个用于实现更透明的效果)
+        LinearGradient linearGradient = new LinearGradient(0, 0, outWidth, 0,
+                new int[]{Color.BLACK, Color.TRANSPARENT, Color.TRANSPARENT}, new float[]{0f, 0.8f, 1f}, Shader.TileMode.CLAMP);
+        LinearGradient linearGradient2 = new LinearGradient(outWidth * 3 / 4, 0, outWidth, 0,
+                Color.BLACK, Color.TRANSPARENT, Shader.TileMode.CLAMP);
+        ComposeShader composeShader = new ComposeShader(linearGradient, linearGradient2, PorterDuff.Mode.DST_IN);
+        paint.setShader(composeShader);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
+        canvas.drawRect(0, 0, outWidth, outHeight, paint);
+        return result;*/
     }
 }
